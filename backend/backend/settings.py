1a159bb90e1b365b8fe0ae3394f10e4b60bccd2e
Rupesh <dasrupesh2124@gmail.com>
2026-04-17 10:06:44 +0530

    feat: implement working book listing API with DRF and database integration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'doc_intelligence',
+        'USER': 'root',
+        'PASSWORD': 'rupesh@2124',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- backend/books/models.py
+++ backend/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    rating = models.FloatField(null=True, blank=True)
+    url = models.URLField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
